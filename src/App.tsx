3e33905ae499b92327b858d166897b7f9e35f4e2
a.gebril <a.gebril@metro-markets.de>
2022-09-27 11:36:27 +0200

    feat(app): Init frontend app

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Provider } from "react-redux";
+
+import "./App.css";
+import { Header } from "./library/common/components/Header/Header";
+import { store } from "./main/store/store";
+import { Dashboard } from "./modules/Dashboard/Dashboard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Header />
+        <Dashboard />
+      </div>
+    </Provider>
   );
 }
 
